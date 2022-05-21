ae790245b788d99a7c844321eb4ccafd7dea32d5
triment <lsplub@gmail.com>
2022-05-21 16:06:07 +0800

    all

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 // Modules to control application life and create native browser window
-const {app,Tray, BrowserWindow} = require('electron')
+const {app,Tray, Menu, BrowserWindow} = require('electron')
 const path = require('path')
 //app.dock.bounce()
 //app.dock.setMenu(null)
 //app.dock.setIcon(new Tray(path.join(__dirname,"./logo.png")))
-
+Menu.setApplicationMenu(null)
 function createWindow () {
   // Create the browser window.
-  console.log(new Tray(path.join(__dirname, 'logo.png')))
   const mainWindow = new BrowserWindow({
     width: 1300,
     height: 800,
     title: 'Teachable Machine',
+    frame: false,
+    autoHideMenuBar: true,
     icon: path.join(__dirname, 'logo.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
   mainWindow.loadURL('https://train.aimaker.space/train')
-
+  mainWindow.webContents.addListener('page-title-updated', (event, title, explicitSet)=>{
+    if (title === '英荔 AI 训练平台 | Teachable Machine'){
+      mainWindow.title = "Teachable Machine"
+    }
+  })
   // and load the index.html of the app.
   //mainWindow.loadFile('index.html')
 
--- preload.js
+++ preload.js
@@ -1,66 +1,3 @@
-class Dep { // 订阅池
-  constructor(name) {
-    this.subs = [] //该事件下被订阅对象的集合
-  }
-  defined(watch) {
-    this.subs.push(watch)
-  }
-  notify() { //通知订阅者有变化
-    console.log('this.subs',this.subs)
-    this.subs.forEach((e, i) => {
-      if (typeof e.update === 'function') {
-        try {
-          e.update.apply(e) //触发订阅者更新函数
-        } catch (err) {
-          console.error(err)
-        }
-      }
-    })
-  }
-}
-
-class Watch {
-  constructor(name, fn) {
-    this.name = name; //订阅消息的名称
-    this.callBack = fn; //订阅消息发送改变时->订阅者执行的回调函数     
-  }
-  add(dep) { //将订阅者放入dep订阅池
-    dep.subs.push(this);
-  }
-  update() { //将订阅者更新方法
-    var cb = this.callBack; //赋值为了不改变函数内调用的this
-    cb(this.name);
-  }
-}
-
-function addMethod() {
-  let historyDep = new Dep();
-  return function (name) {
-    if (name === 'historychange') {
-      return function (name, fn) {
-        let event = new Watch(name, fn)
-        historyDep.defined(event);
-      }
-    } else if (name === 'pushState' ) {
-      let method = history[name];
-      return function () {
-        console.log('pushState','arguments',arguments[2])
-        method.apply(history, arguments);
-        historyDep.notify();
-      }
-    }else if ( name === 'replaceState') {
-      let method = history[name];
-      return function () {
-        console.log('replaceState','arguments',arguments[2])
-        method.apply(history, arguments);
-        historyDep.notify();
-      }
-    }
-
-  }
-}
-         
-
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -72,11 +9,10 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
-  
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.title = "Teachable Machine";
+  //document.title = "Teachable Machine";
   document.querySelector('link[rel="shortcut icon"]').href = "";
   const timer = setInterval(() => {
     if (document.querySelector('#codelab-tips') != null) {
